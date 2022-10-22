93abe77e089222ff2665e85a824988768d877489
Timosha <flykaiq449@gmail.com>
2022-10-22 14:23:09 +0300

    feat: add all progect

---
+++ stylehelper.h
@@ -0,0 +1,36 @@
+#ifndef STYLEHELPER_H
+#define STYLEHELPER_H
+
+#include <QString>
+
+class StyleHelper
+{
+public:
+    static QString getMainWidgetStyle();
+    static QString getStartButtonStyle();
+    static QString getLeftButtonStyle();
+    static QString getRightButtonStyle();
+    static QString getLeftButtonActiveStyle();
+    static QString getRightButtonActiveStyle();
+    static QString getTabWidgetStyle();
+    static QString getTabStyle();
+    static QString getStartButtonGameStyle();
+
+    static QString getBlankButtonStyle();
+    static QString getCrossNormalStyle();
+    static QString getCrossVictoryStyle();
+    static QString getCrossLostStyle();
+
+    static QString getZeroNormalStyle();
+    static QString getZeroVictoryStyle();
+    static QString getZeroLostStyle();
+
+    static QString getNormalMessageStyle();
+    static QString getVictoryMessageStyle();
+    static QString getLostMessageStyle();
+
+    static QString getAboutTextStyle();
+
+};
+
+#endif // STYLEHELPER_H
